89ae777f4f1d1359650fe9063795f8157828b946
Md. Sabbir Hossain <82496216+SABBIR-H0SSAIN@users.noreply.github.com>
2025-11-14 23:45:37 +0600

    Updated HomePageController and its view

--- src/main/java/com/example/sabbir_2207102_cvbuilder/controllers/HomePageController.java
+++ src/main/java/com/example/sabbir_2207102_cvbuilder/controllers/HomePageController.java
@@ -13,10 +13,10 @@ import java.io.IOException;
 public class HomePageController{
     private Button goToCvFormPageButton;
     public void goToCVFormPageAction(ActionEvent event) throws IOException {
-            Parent createRoot = FXMLLoader.load(getClass().getResource("/com/example/sabbir_2207102_cvbuilder/views/create_cv_form.fxml"));
-            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            stage.setScene(new Scene(createRoot));
-            stage.show();
+        Parent createRoot = FXMLLoader.load(getClass().getResource("/com/example/sabbir_2207102_cvbuilder/views/create_cv_form.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(createRoot));
+        stage.show();
 
     }
 }
